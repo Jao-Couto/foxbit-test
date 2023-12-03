3b7de957dd330104d910c45e2a21ffe2dcc8d640
João Couto <joao.oliveira@fachiniit.com>
2023-12-03 15:23:42 -0300

    Comentários no arquivo index

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,23 @@ import { Plateau } from './plateau/plateau';
 import { InputService } from './input/input-service';
 import { Rover } from './rover/rover';
 
+// Read input file
 const fileContent = InputService.readFile('src/input.txt');
 
+// Get plateau
 const plateau: Plateau = InputService.getPlateau(fileContent[0]);
 
+// Get rovers initial positon and instructions
 const roversConfig = fileContent.slice(1);
+
+// For each 2 lines of the input file, create a rover and move it
 for (let i = 0; i < roversConfig.length; i += 2) {
+	// Create rover
 	const rover: Rover = InputService.getRover(roversConfig[i], plateau);
+
+	// Move rover
 	const roverFinalPosition: string = rover.move(roversConfig[i + 1]);
+
+	// Print rover final position
 	console.log(roverFinalPosition);
 }
